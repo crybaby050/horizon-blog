fa5850f1118513a3873f6bc8467a8094b905c839
crybaby05 <benthiam750@gmail.com>
2026-05-24 11:03:19 +0000

    initialisation du controller de l'acteur lecteur

---
+++ controller/lecteurController.php
@@ -0,0 +1,17 @@
+<?php
+require_once ROOT."/model/lecteurModel.php";
+
+$actions=[
+    "home"=>$home,
+    "categorie"=>$categorie,
+    "article"=>$article,
+    "contact"=>$contact
+];
+$action=$_REQUEST["action"]??"home";
+
+if (array_key_exists($action, $actions)) {
+    $actions[$action]();
+}else{
+    echo "page introuvable ";
+    exit();
+}
